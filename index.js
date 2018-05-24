dd99b670a98c3e40f1ca3edeee6d65ee0e3eaeb8
iomegak12 <jd.ramkumar@gmail.com>
2018-05-24 15:08:22 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 let Customer = require('./models').Customer;
 let CustomerService = require('./services').CustomerService;
 
-let serviceObject = new CustomerService();
-let searchString1 = 'wind';
-let searchString2 = 'work';
 
-let windPromise = serviceObject.getCustomers(searchString1);
-let workPromise = serviceObject.getCustomers(searchString2);
+// Promise
+//     .all([windPromise, workPromise])
+//     .then(
+//         results => {
+//             console.log('Results ...');
 
-Promise
-    .all([windPromise, workPromise])
-    .then(
-        results => {
-            console.log('Results ...');
+//             for (let result of results) {
+//                 for (let customer of result) {
+//                     console.log(customer.toString());
+//                 }
 
-            for (let result of results) {
-                for (let customer of result) {
-                    console.log(customer.toString());
-                }
+//                 console.log('===============================');
+//             }
+//         },
+//         error => console.log('One of the promises failed!'));
 
-                console.log('===============================');
+async function process() {
+    try {
+        let serviceObject = new CustomerService();
+        let searchString1 = 'wind';
+        let searchString2 = 'work';
+
+        let windPromise = serviceObject.getCustomers(searchString1);
+        let workPromise = serviceObject.getCustomers(searchString2);
+        let results = await Promise.all([windPromise, workPromise]);
+
+        for (let result of results) {
+            for (let customer of result) {
+                console.log(customer.toString());
             }
-        },
-        error => console.log('One of the promises failed!'));
+
+            console.log('===============================');
+        }
+    } catch (error) {
+        console.log(`Error Occurred, Details : ${JSON.stringify(error)}`);
+    }
+}
+
+process()
+    .then(() => console.log('Continuing the work ...'));
